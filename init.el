4545a3c248ab12ceac93a4079c1b35523396ec47
tasasei <taiqinga@gmail.com>
2017-07-07 15:42:46 +0900

    Create init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+;; Alt + 矢印でウィンドウを移動する
+(windmove-default-keybindings 'M) ; Alt の場合は meta を指定
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ 
